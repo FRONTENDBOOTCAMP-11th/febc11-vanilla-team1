319fc182db183c4a67b607806c3d3f2a1fbd6f97
kim-young-dong <opa9904@gmail.com>
2024-10-16 13:34:19 +0900

    init: CSS 초기화

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#9e3500',
+        secondary: '#007d48',
+        'gray-100': '#f5f5f5',
+        'gray-200': '#e5e5e5',
+        'gray-300': '#cacacb',
+        'gray-400': '#9e9ea0',
+        'gray-500': '#707072',
+        black: '#111111',
+        white: '#ffffff',
+        dim: 'rgba(17, 17, 17, 0.35)',
+      },
+    },
   },
   plugins: [],
 };
