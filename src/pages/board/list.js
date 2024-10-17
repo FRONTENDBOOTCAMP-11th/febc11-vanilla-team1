be8ef9c952c04384fdd8759f0a12f798bc0d2c03
kim-young-dong <opa9904@gmail.com>
2024-10-17 13:19:14 +0900

    feat: 정렬 드롭다운 메뉴 추가, 모바일 카테고리 구현, 서브타이틀 추가

---
+++ src/pages/board/list.js
@@ -0,0 +1,67 @@
+'use strict';
+import axios from 'axios';
+
+// 필터 토글
+function filterToggle(e) {
+  e.preventDefault();
+
+  const asideNode = document.querySelector('.sidebar');
+  const toggleTextNode = document.querySelector('.filter-toggle span');
+  asideNode.classList.toggle('active');
+  toggleTextNode.textContent = toggleTextNode.textContent.includes('숨기기')
+    ? '필터 표시'
+    : '필터 숨기기';
+}
+document
+  .querySelector('button.filter-toggle')
+  .addEventListener('click', filterToggle);
+
+// 정렬 드롭다운
+function sortMenuDropdown(e) {
+  e.preventDefault();
+
+  const dropdownNode = document.querySelector('.dropdown__options-list');
+  dropdownNode.classList.toggle('active');
+  const buttonIcon = document.querySelector('button.sort-toggle img');
+  buttonIcon.src = buttonIcon.src.includes('up')
+    ? '../../assets/icons/button24px/down.svg'
+    : '../../assets/icons/button24px/up.svg';
+}
+document
+  .querySelector('button.sort-toggle')
+  .addEventListener('click', sortMenuDropdown);
+
+// 게시글 목록 조회
+async function getList() {
+  const res = await axios.get('https://11.fesp.shop/posts', {
+    params: {
+      type: 'test',
+    },
+  });
+  return res.data;
+}
+
+// 게시글 출력
+async function renderList() {
+  const list = getList();
+  if (!list) {
+    return;
+  }
+
+  const { item } = await getList();
+
+  // type Post = {
+  //   _id: string;
+  //   title: string;
+  //   user: {
+  //     _id: string;
+  //     name: string;
+  //   };
+  //   content: string;
+  //   views: number;
+  //   createdAt: string;
+  // };
+  console.log(item);
+}
+
+// renderList();
