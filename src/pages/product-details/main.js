ce8d8f5ae3c6cc7e45db46d7e131cac391eb1526
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-10-24 18:38:41 +0900

    script module 작성

--- src/pages/product-details/main.js
+++ src/pages/product-details/main.js
@@ -1,5 +1,31 @@
 'use strict';
 
+// eslint-disable-next-line no-undef
+new Swiper('.mySwiper', {
+  slidesPerView: 3,
+  spaceBetween: 10,
+  loop: false,
+  autoHeight: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.right_button',
+    prevEl: '.left_button',
+  },
+  breakpoints: {
+    500: {
+      slidesPerView: 1,
+      spaceBetween: 40,
+    },
+    959: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+  },
+});
+
 // Panel on off
 var act = document.getElementsByClassName('active_button');
 var val; //변수 val
@@ -16,4 +42,8 @@ for (val = 0; val < act.length; val++) {
       shoes_panel.style.maxHeight = shoes_panel.scrollHeight + 'px';
     }
   });
+}
+function toggleImg() {
+  document.getElementsByClassName('nike_shoes')[0].src =
+    '../../../public/assets/icons/shoes/(big)NIKE+JAM_black.png';
 }
