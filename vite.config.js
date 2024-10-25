ba1e7a8a89d7a52b6be68a3394e5511ac4382a30
doyoung1002 <leedoyoung.oc@gmail.com>
2024-10-25 20:09:01 +0900

    docs: config 파일 수정

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,11 @@ export default defineConfig({
         header: 'src/components/header/header.html',
         footer: 'src/components/footer/footer.html',
         index: 'index.html', // 기본 index.html
+        sign: 'src/pages/auth/signup.html',
         login: 'src/pages/auth/login.html', // 추가 HTML 파일
+        pw: 'src/pages/auth/pw.html',
+        check: 'src/pages/auth/check.html',
         list: 'src/pages/product/list/index.html', // 추가 HTML 파일
-        // 필요한 다른 HTML 파일을 여기에 추가
       },
     },
   },
