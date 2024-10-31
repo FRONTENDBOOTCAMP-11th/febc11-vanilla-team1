9882a816cc70247e1cb9f2515dc6e5b968de1b6a
baxteryhs1118 <baxteryhs1118@gmail.com>
2024-10-31 16:31:51 +0900

    api 진행중

--- src/pages/product/detail/main.js
+++ src/pages/product/detail/main.js
@@ -89,12 +89,40 @@ document.querySelector('.black_button').addEventListener('click', function () {
   imgchange_black.setAttribute('src', 'https://via.placeholder.com/544');
 });
 
-//----------------
+//-----------------------------------------------------------------------------
+// let product_item = {
+//   name: '나이키 잼',
+//   _id: 1,
+//   price: 125100,
+// };
 
-const urlSearch = new URLSearchParams(location.search);
-const productId = urlSearch.get('productId');
+// const res = await fetch('https://11.fesp.shop/products/1');
+// console.log(res);
 
-const basket = document.querySelector('.basket');
+// fetch('https://11.fesp.shop/products/1')
+//   .then(response => response.json())
+//   .then(data => ['name']);
+
+// fetch('https://11.fesp.shop/products/1')
+//   .then(response => response.json())
+//   .then(data => console.log(data['name']));
+
+// res 변수에 json이라는 메서드를 사용해 이를 객체형태로 변환하고
+// fetch함수는 비동기적으로 처리되기 때문에
+// 이 api호출이 완전히 끝난 이후
+// res변수를 객체로 변환해주기 위해 await를 사용한다.
+// 결과에 데이터들이 객체 형태로 떠야 innerHTML로 뭘 하든 할텐데...
+const getData = async () => {
+  const res = await api('get', `products/1`);
+  console.log(res.data.item);
+  console.log(res.data.item['name']);
+  // console.log(res.data.item['options'][0].extra.size);
+  document.getElementById('shoes_name').innerHTML = res.data.item['name'];
+};
+getData();
+
+const url = new URLSearchParams(location.search);
+const productId = url.get('productId');
 
 const getProduct = async function (productId) {
   try {
@@ -107,56 +135,17 @@ const getProduct = async function (productId) {
 };
 const product = await getProduct(productId);
 
-const renderImage = function () {
+const renderImage = function (product) {
   const shoes_img = document.querySelector('.nike_shoes');
-  shoes_img.innerHTML = '';
-  if (product.item.options.length === 0) {
-    product.item.mainImages.map(
-      e =>
-        (shoes_img.innerHTML += `<img src='https://11.fesp.shop/files/vanilla01/${e.name}
-      ' />`),
-    );
-  } else {
-    product.item.options[currentOption.option].mainImages.map(
-      e =>
-        (shoes_img.innerHTML += `<img src='https://11.fesp.shop/files/vanilla01/${e.name}
-' />`),
-    );
-  }
+  const img = product.item.mainImages[0];
+  shoes_img.src = `https://11.fesp.shop${img.path}`;
+  // console.log(shoes_img.src);
 };
+renderImage(product);
 
-const renderSize = function () {
-  const sizelist = document.querySelector('.det_size_list');
-  sizelist.innerHTML = '';
+const currentOption = function () {};
 
-  if (product.item.options.length === 0) {
-    product.item.extra.size.map(e => {
-      sizelist.innerHTML += `<span>${e}</span>`;
-    });
-  } else {
-    product.item.options[currentOption.option].extra.size.map(e => {
-      sizelist.innerHTML += `<span>${e}</span>`;
-    });
-  }
-
-  const sizes = [...sizelist.querySelectorAll('span')];
-  sizes.forEach(e => {
-    e.addEventListener('click', function (e) {
-      currentOption.size = e.target.textContent;
-
-      [...sizelist.querySelectorAll('span')].map(e => {
-        e.classList.remove('unactive');
-      });
-      e.target.classList.add('active');
-    });
-  });
-};
-
-const currentOption = { option: 0, size: null };
-
-renderImage(product, currentOption);
-renderSize(product, currentOption);
-
+const basket = document.querySelector('.basket');
 basket.addEventListener('click', async function () {
   console.log(productId, currentOption.size);
   if (currentOption.size === null) {
